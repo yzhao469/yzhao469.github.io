e499872b37d073169ea91a0dc99bae4e7c83e675
yzhao469 <79812633+yzhao469@users.noreply.github.com>
2025-09-13 22:29:06 +0000

    Deploying to gh-pages from @ yzhao469/yzhao469.github.io@aaf47537642ca764a73a724de0814eef8e9645b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-yumeng-has-officially-started-his-new-job-as-an-assitant-professor-at-unl",
+          title: 'Yumeng has officially started his new job as an assitant professor at UNL....',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
