44695db56559bf1ff233306839745a04a317f642
yzhao469 <79812633+yzhao469@users.noreply.github.com>
2025-09-13 21:54:26 +0000

    Deploying to gh-pages from @ yzhao469/yzhao469.github.io@59e92450ef76027238bd6dcfe9d068084faff721 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "At Georgia Institute of Technology,CEE 4405 Introduction to Geotechnical Engineering,Summer 2023, undergraduate level,This course covers the fundamentals of soil properties and geotechnical engineering practice. Specific topics includes soil characterization and classification; compaction and soil improvement; fluid flow through porous media; stresses and strains in soils; settlement and consolidation analyses; shear strength and earth retaining structures. The course includes five laboratory sessions that provide students with hands-on experience on geotechnical materials. Syllabus - CEE 4405 Intro to Geotech Eng",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
