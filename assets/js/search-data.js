5a348834c9a5c199a5e7eb61cc793d4df824c555
yzhao469 <79812633+yzhao469@users.noreply.github.com>
2025-09-13 21:54:55 +0000

    Deploying to gh-pages from @ yzhao469/yzhao469.github.io@68b599f4784bf0d77066f4a30816d461d5456d73 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "At Georgia Institute of Technology,CEE 4405 Introduction to Geotechnical Engineering,Summer 2023, undergraduate level,This course covers the fundamentals of soil properties and geotechnical engineering practice. Specific topics includes soil characterization and classification; compaction and soil improvement; fluid flow through porous media; stresses and strains in soils; settlement and consolidation analyses; shear strength and earth retaining structures. The course includes five laboratory sessions that provide students with hands-on experience on geotechnical materials. Syllabus - CEE 4405 Intro to Geotech Eng",
+          description: "At the University of Nebraska, Lincoln,MECH 325 Mechanics of Elastic Bodies,Fall 2025, undergraduate level,This course introduces the mechanics of solid materials within the range of elastic deformation with applications to science and engineering. Topics covered include static equilibrium, free body diagrams, analysis of stresses, strains and deformation in structural elements (axial, torsional and bending), states of stress (shear, bending, torsion), shear and moment diagrams for beams, displacements and deformations, column buckling, Mohr’s Circle, failure theories etc. Syllabus - MECH 3250 Mechanics of Elastic Bodies",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
